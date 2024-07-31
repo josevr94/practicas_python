7fa497b129c9202d3f62ea7cfaa092ce41601cf2
josevr94 <Josevrw1@gmail.com>
2024-07-31 10:55:58 -0400

    subiendo la primera actualizacion python

---
+++ variables.py
@@ -0,0 +1,24 @@
+#variables d e tipo etero
+a= 10
+b= 10
+print (a)
+print (b)
+
+#variables flotantes
+c= 1.6
+d= 2.5
+suma= c + d
+print (suma)
+
+#variable tipo string
+nombre = "jose"
+apellido= "viñuela"
+full_name= nombre + " " +apellido
+print (full_name)
+
+#variable de tipo boolean
+lampara = True
+print (lampara)
+lampara= False
+print ("la lampara esta apagada ",  lampara)
+##########g
